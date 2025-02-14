791e19061b969ba39611c7ddafe45a4da234d3bd
Ruslan Hutsul <ruslanhutsul343@gmail.com>
2025-02-14 20:01:41 +0200

    added loop and logic for user input

--- src/main/java/org/hutsdev/Main.java
+++ src/main/java/org/hutsdev/Main.java
@@ -1,7 +1,39 @@
 package org.hutsdev;
 
+import org.hutsdev.service.GitHubService;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        GitHubService gitHubService = new GitHubService();
+
+        System.out.println("Choose command below:");
+        System.out.println("1. github-activity <username>\n" +
+                           "2. exit");
+
+        while (true) {
+            System.out.print("> ");
+            String option = scanner.nextLine();
+            String[] commandArgs = option.split(" ", 2);
+
+            String command = commandArgs[0];
+            switch (command) {
+                case "github-activity" -> {
+                    if (commandArgs.length < 2) {
+                        System.out.println("Usage: github-activity <username>");
+                    }
+                    else {
+                        gitHubService.fetchUserActivity(commandArgs[1]);
+                    }
+                }
+                case "exit" -> {
+                    System.out.println("Exiting program...");
+                    scanner.close();
+                    return;
+                }
+                default -> System.out.println("Unknown command! Please, try again.");
+            }
+        }
     }
 }
